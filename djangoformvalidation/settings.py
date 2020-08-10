466f85757ef1b1e8cb054bd103ed47666db064cf
Ansari Arshaduddin <ansariarshaduddin@gmail.com>
2020-08-10 17:00:46 +0530

    commit

--- djangoformvalidation/settings.py
+++ djangoformvalidation/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TAMELATES_DIR=os.path.join(BASE_DIR,'templates')
-STATIC_DIR=os.path.join(BASE_DIR,'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -124,7 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[STATIC_DIR,]
 
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
